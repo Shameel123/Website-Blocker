2d11a6420ccfd103943a69e53109e92116d3b4da
Shameel <shameel_uddin@yahoo.com>
2020-07-05 23:42:27 +0500

    Working fine till here, going to extend it later on.

---
+++ code.py
@@ -0,0 +1,36 @@
+import time
+from datetime import datetime as dt
+#time.sleep(5) #-->Runs after 5 seconds
+
+
+# host_path = r"C:\Windows\System32\drivers\etc\hosts"
+#remove above comment if you want to seriously run this :-)
+host_path = "hosts"
+redirect="127.0.0.100"
+website_list = ["www.facebook.com","facebook.com","gmail.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working Hours")
+        with open(host_path,"r+") as file: #r+ helps to read as well as write.
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+        time.sleep(5)
+        file.close()
+
+    else:
+        print("Not working hours")
+        with open(host_path,"r+") as file: #r+ helps to read as well as write.
+            content = file.readlines()
+            file.seek(0)#it is necessary to place pointer at the very first of the file.
+            for line in content:
+                if not any(website in line for website in website_list):
+                    # if www.facebook.com is not in first line of hosts then write first line of host on host same to same.
+                    file.write(line)
+            file.truncate
+        time.sleep(5)
+        file.close()
---
+++ readme.txt
@@ -0,0 +1,13 @@
+the code shuts down the websites from 0800 hours to 1600 hours.
+modify the code as directed in comments.
+
+run the script with administrative access. --> open cmd with administrator and then run it.
+
+------------------------
+To run this in background process :
+
+change the extension from 'py' to 'pyw' --> "code.py" to "code.pyw"
+then simply open it with ADMINISTRATIVE ACCESS 
+--> blocker.pyw is an example. Just run it with administrative priveleges.
+
+Schedule it task scheduler -> see it from google or youtube.
